cc3900d7dcb5e8ea121c9e19ced20e05870b9468
Pedro marchese <pedro.marchese03@gmail.com>
2026-09-11 11:12:28 -0300

    chore: limpar main

--- src/main/java/br/com/fiap/main/Main.java
+++ src/main/java/br/com/fiap/main/Main.java
@@ -1,11 +1,12 @@
 package br.com.fiap.main;
 
-import br.com.fiap.utils.Jopt;
+import br.com.fiap.dao.UserDao;
+
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] test = {"Login", "cadastro"};
+    public static void main(String[] args){
 
-        System.out.println(Jopt.showOption(test));
+
     }
 }
