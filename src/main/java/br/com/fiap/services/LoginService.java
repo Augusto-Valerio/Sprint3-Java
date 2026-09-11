6d6f93cb5412cd5bde33afc68d8f8814ead366f8
Augusto Valerio <augusto.valerio06@gmail.com>
2026-09-11 20:08:35 -0300

    feat(login): adicionar login service

---
+++ src/main/java/br/com/fiap/services/LoginService.java
@@ -0,0 +1,26 @@
+package br.com.fiap.services;
+
+import br.com.fiap.dao.LoginDao;
+import br.com.fiap.entities.User;
+
+import java.sql.SQLException;
+
+public class LoginService {
+
+    public User Login(String userName, String password) throws SQLException, ClassNotFoundException {
+        LoginDao loginDao = new LoginDao();
+
+        // Busca o usuario no banco de dados
+        User user = loginDao.findUserByName(userName);
+
+        if (user == null) {
+            throw new IllegalArgumentException("Usuario não encontrado");
+        }
+
+        if (!user.getUserPassWord().equals(password)) {
+            throw new IllegalArgumentException("Senha incorreta");
+        }
+
+        return user;
+    }
+}
