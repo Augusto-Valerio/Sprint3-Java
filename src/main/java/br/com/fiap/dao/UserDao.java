e61ecb3decbc6be235d6675646b8fb844b1ae457
Augusto Valerio <augusto.valerio06@gmail.com>
2026-09-11 21:46:54 -0300

    feat(login): adicionar busca de user por username

--- src/main/java/br/com/fiap/dao/UserDao.java
+++ src/main/java/br/com/fiap/dao/UserDao.java
@@ -27,4 +27,28 @@ public class UserDao extends FactoryDao {
 
         return id;
     }
+
+    public User findByUserName(String userName) throws SQLException {
+        ResultSet rs = select(
+                "SELECT USER_ID, USER_NAME, USER_PASSWORD FROM USUARIO WHERE USER_NAME = ?",
+                userName
+        );
+
+        if (rs.next()) {
+            User user = new User(
+                    rs.getString("USER_NAME"),
+                    rs.getString("USER_PASSWORD")
+            );
+
+            user.setUserId(rs.getInt("USER_ID"));
+
+            rs.close();
+
+            return user;
+        }
+
+        rs.close();
+
+        return null;
+    }
 }
